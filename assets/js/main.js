0044f3e02dcbb65c6b4944c9aa9114d236edae1d
Omar Khuatbyek <omar.khuatbek@gmail.com>
2023-10-28 18:24:19 -0500

    wrapped the buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,7 +213,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 800,
       easing: 'ease-in-out',
       once: false,
       mirror: false
